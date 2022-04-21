b263b04bd5a4469d57104b86d5b643b6c037b912
Roger Grange <rgrang816@west-mec.local>
2022-04-20 18:04:39 -0700

    working on styling

--- MyCompany/javascript/orderList.js
+++ MyCompany/javascript/orderList.js
@@ -1,6 +1,7 @@
 //the current cart, holds what you are buying
 class cart{
     constructor(cartElement){
+        this.currentCost=0;
         this.items={}
         this.cartElement=cartElement;
     }
@@ -14,6 +15,7 @@ class cart{
         this.updateCart()
     }
     removeItem(item_name){
+        if(!Object.keys(this.items).includes(item_name)){return}
         this.items[item_name][0]--
         this.items[item_name][1].innerHTML=this.items[item_name][0]+" "+item_name
         if(this.items[item_name][0]<=0){
@@ -24,17 +26,42 @@ class cart{
     }
     //this will calculate cost
     updateCart(){
+        this.currentCost=5;
+        //goes through both sale sections to add the total cost
+        for(let [item,value] of Object.entries(this.items)){
+            if(Object.keys(forSale).includes(item)){
+                this.currentCost+=forSale[item]*value[0]
+            }else if(Object.keys(regularSale).includes(item)){
+                this.currentCost+=regularSale[item]*value[0]
+            }
+        }
+        let currencyForm=this.makeCurrencyForm(this.currentCost)
+        document.getElementById("costLabel").innerHTML="Cost: "+currencyForm
+        if(parseFloat(currencyForm.split("$")[0])>125.00){
+            alert("The order has exceeded 125$, Please contact us directly or by phone to place orders this large")
+        }
     }
     //creates the item element to add to your cart zone
     createItem(name,quantity){
         let item=document.createElement("li")
         item.setAttribute("value",name)
         item.innerHTML=quantity+" "+name
-        item.onclick=()=>myCart.removeItem(name)
+        
         this.cartElement.appendChild(item)
         return item
     }
-    
+    makeCurrencyForm(num){
+        let numString=String(num)
+        if(!numString.includes(".")){numString+=".00"}
+        //makes sure there are enough zeroes at the end
+        while(numString.split(".")[1].length<2){
+            numString+="0"
+        }
+        while(numString.split(".")[1].length>2){
+            numString=numString.substr(0,numString.length-1)
+        }
+        return numString+"$"
+    }
 }
 let myCart=new cart(document.getElementById("purchasedItems"));
 
@@ -56,6 +83,7 @@ for(const item of Object.keys(regularSale)){
 let itemHolder=document.createElement("p")
 itemHolder.innerHTML=item
 itemHolder.appendChild(createAddButton(item))
+itemHolder.appendChild(createSubButton(item))
 
 itemHolder.className="sellingItem"
 regular.appendChild(itemHolder)
@@ -70,5 +98,12 @@ function createAddButton(item_name){
     btn.className="addButton"
     btn.onclick=()=>myCart.addItem(item_name)
     btn.innerHTML="+"
+    return btn
+}
+function createSubButton(item_name){
+    let btn=document.createElement("button")
+    btn.className="subButton"
+    btn.onclick=()=>myCart.removeItem(item_name)
+    btn.innerHTML="-"
     return btn
 }
