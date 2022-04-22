a91aeb26817eb7b51c8c96d88171f14b9683ac18
Roger Grange <rgrang816@west-mec.local>
2022-04-21 17:43:57 -0700

    a

---
+++ MyCompany/javascript/availableProducts.js
@@ -0,0 +1,16 @@
+//this is for adding the items to the order list
+const forSale={
+    "Matcha Mochi":10.95,
+    "Italian tiramisu":11.95,
+    "cannoli":8.95,
+    "Eclairs":8.95,
+    "churros 3pc":6.95,
+}
+const regularSale={
+    "Chocolate Fudge":5.95,
+    "White Chocolate Fudge":5.95,
+    "Apple Pie":15.95,
+    "Brownies 6''":6.95,
+    "Cupcakes":12.95,
+    
+}
