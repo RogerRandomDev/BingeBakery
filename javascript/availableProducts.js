9d981abd045a6d503b793dc93238301fe5454990
Roger Grange <rgrang816@west-mec.local>
2022-05-18 18:02:12 -0700

    fixed missing files

---
+++ javascript/availableProducts.js
@@ -0,0 +1,17 @@
+const forSale={
+    "Mochi 6pc":10.95,
+    "Italian tiramisu":11.95,
+    "cannoli":8.95,
+    "Eclairs":8.95,
+    "churros 3pc":6.95,
+     "chocolate truffles 8pc":12.95,
+     "Maple butter tarts 4pc":11.95,
+     "macaroons 8pc":10.95,
+}
+const regularSale={
+    "Chocolate Fudge":5.95,
+    "White Chocolate Fudge":5.95,
+    "Apple Pie":15.95,
+    "Brownies 6''":6.95,
+    "Cupcakes":12.95,
+}
